3c99416d44e0844ebe27c83e5fb300015f6874ee
Abhilash A B <abhilashab461@gmail.com>
2026-02-27 22:52:19 +0530

    updated import in sns.js

--- backend/src/services/sns.js
+++ backend/src/services/sns.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express = require('express');
-const { seedData } = require('./models/db');
+const { seedData } = require('../models/db');
 
 const app = express();
 app.use(express.json());
